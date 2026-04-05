0ddeb684e83beb7c63d8375165dd3c2841cc55eb
imhsart <nitinrana2519@gmail.com>
2026-04-05 13:56:43 +0530

    assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <ol key={relativeList}>
+                <li key={relativeListItem1}>Closest relative</li>
+                <li key={relativeListItem2}>Second closest relative</li>
+                <li key={relativeListItem3}>Third closest relative</li>
+                <li key={relativeListItem4}>Second nearest relative</li>
+                <li key={relativeListItem5}>Closest relative</li>
+               </ol>
     </div>
   )
 }
